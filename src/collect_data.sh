bd959a6e01f2072b0bae176895eeb78d2c7bef5a
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-02-12 13:35:41 -0500

    feat: add calme alias for gcalcli with multiple calendars

--- src/collect_data.sh
+++ src/collect_data.sh
@@ -1,4 +1,5 @@
 # calendar command: calme agenda
+alias calme=gcalcli --calendar "Personal/Social" --calendar "abuzarmahmood@gmail.com" --calendar "abuzarmahmood@brandeis.edu" --calendar "Course Timetable"
 
 # New dedicated path for calendar logs
 CALENDAR_LOG_PATH="/home/abuzarmahmood/Desktop/calendar_log"
